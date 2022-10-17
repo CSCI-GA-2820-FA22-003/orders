c2c7c3147f6238222c23333d0af4917d98073c19
Yintao, Xu <yx2412@nyu.edu>
2022-10-17 00:49:37 +0000

    [Update] Remove dependency on customer model , solve some minor issue.

--- service/routes.py
+++ service/routes.py
@@ -6,7 +6,8 @@ Describe what your service does here
 
 from flask import Flask, jsonify, request, url_for, make_response, abort
 from .common import status  # HTTP Status Codes
-from service.models import YourResourceModel
+from service.models import Item
+from service.models import Order
 
 # Import Flask application
 from . import app
@@ -32,4 +33,5 @@ def index():
 def init_db():
     """ Initializes the SQLAlchemy app """
     global app
-    YourResourceModel.init_db(app)
+    Item.init_db(app)
+    Order.init_db(app)
