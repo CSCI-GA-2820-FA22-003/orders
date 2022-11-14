cc9d129c756d0637524923b2e79fbfc6fbfedc7c
VidishaShah29 <vidishashah29@yahoo.com>
2022-11-14 14:26:35 -0500

    Basic UI features

--- service/routes.py
+++ service/routes.py
@@ -1,7 +1,12 @@
 """
 My Service
 
-Describe what your service does here
+The orders resource is a collection of order items where each item represents a 
+product id, its quantity, and its price. 
+We also implement a subordinate REST API to add order items to the order collection 
+(e.g., /orders/{id}/items) and associate the order with a customer preferably through 
+its customer id. A good action for the order API is to be able to cancel an order.
+
 """
 
 from flask import jsonify, request, url_for, abort
