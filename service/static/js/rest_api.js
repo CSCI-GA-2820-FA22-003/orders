f6626787ab07e09f16a71d24deadb05ec7a18263
Yintao, Xu <yx2412@nyu.edu>
2022-11-27 01:34:14 +0000

    [Add] UI for updating order

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -40,4 +40,35 @@ $(function () {
             flash_message(res.responseJSON.message)
         });
     });
+
+    // Update an Order
+    $("#update-btn").click(function () {
+
+        var order_id = $("#update_order_id").val();
+        var name = $("#update_order_name").val();
+        var address = $("#update_order_addr").val(); 
+
+        var data = {
+            "id": order_id,
+            "name": name,
+            "address": address, 
+        };
+        console.log(data);
+
+        var ajax = $.ajax({
+            type: "PUT",
+            url: "/api/orders/" + order_id,
+            contentType: "application/json",
+            data: JSON.stringify(data),
+        });
+
+        ajax.done(function (res) {
+            flash_message("Success ")
+            update_form_data(JSON.stringify(res))
+        });
+
+        ajax.fail(function (res) {
+            flash_message(res.responseJSON.message)
+        });
+});
 })
