4e38c7f9f204cc1071b57dfc59fa69180b0418f2
VidishaShah29 <vidishashah29@yahoo.com>
2022-11-29 15:31:23 -0500

    Update rest_api.js

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -70,5 +70,40 @@ $(function () {
         ajax.fail(function (res) {
             flash_message(res.responseJSON.message)
         });
-});
+    });
+    
+    // List all Orders
+    $("#listall-btn").click(function () {
+
+        var ajax = $.ajax({
+            type: "GET",
+            url: "/api/orders",
+            data: ''
+        })
+
+        ajax.done(function (res) {
+            //alert(res.toSource())
+            $("#list_results").empty();
+            $("#list_results").append('<table class="table-striped" cellpadding="10">');
+            var header = '<tr>'
+            header += '<th">id</th>'
+            header += '<th">name</th>'
+            header += '<th">address</th>'
+            header += '<th">date_created</th></tr>'
+            $("#list_results").append(header); 
+            for (var i = 0; i < res.length; i++) {
+                var order = res[i];
+                var row = "<tr><td>" + order.id + "</td><td>" + order.name + "</td><td>" + order.address + "</td><td>" + order.date_created + "</td></tr>";
+                $("#list_results").append(row);
+            }
+            $("#list_results").append('</table>');
+
+            flash_message("Success")
+        });
+
+        ajax.fail(function (res) {
+            flash_message(res.responseJSON.message)
+        });
+
+    });
 })
